48365c0639cb47427b1fd423cf760e4a161e8f8c
qorcodusl <qorcodus03@naver.com>
2025-10-23 22:46:35 +0900

    feat(domain) : 랜덤 이동 전략 구현

---
+++ src/main/java/racingcar/domain/RandomMovementStrategy.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomMovementStrategy implements MovementStrategy {
+
+    private static final int MIN_MOVABLE_NUMBER = 4;
+
+    @Override
+    public boolean isMovable() {
+        int randomNumber = Randoms.pickNumberInRange(0, 9);
+        return randomNumber >= MIN_MOVABLE_NUMBER;
+    }
+}
