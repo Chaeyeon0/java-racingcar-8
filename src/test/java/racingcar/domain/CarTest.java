3858ea33baaa32fac8c6727a2067ca3366f5756a
qorcodusl <qorcodus03@naver.com>
2025-10-23 22:08:33 +0900

    fix(domain) : @DisplayName으로 테스트 의도 설명

--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -8,21 +9,24 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class CarTest {
 
     @Test
-    void 이름이_5자를_초과하면_예외가_발생한다() {
+    @DisplayName("자동차 이름이 5자를 초과하면 예외를 발생시킨다")
+    void throwsExceptionWhenNameExceedsFiveCharacters() {
         assertThatThrownBy(() -> new Car("abcdef"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("5자 이하");
     }
 
     @Test
-    void 이름이_비어있으면_예외가_발생한다() {
+    @DisplayName("자동차 이름이 비어있으면 예외를 발생시킨다")
+    void throwsExceptionWhenNameIsBlank() {
         assertThatThrownBy(() -> new Car(" "))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("비어 있을 수 없습니다");
     }
 
     @Test
-    void 이동조건이_참이면_전진한다() {
+    @DisplayName("이동 조건이 참일 경우 자동차는 전진한다")
+    void movesForwardWhenStrategyReturnsTrue() {
         Car car = new Car("pobi");
 
         // 항상 이동하는 전략
@@ -37,7 +41,8 @@ class CarTest {
     }
 
     @Test
-    void 이동조건이_거짓이면_정지한다() {
+    @DisplayName("이동 조건이 거짓일 경우 자동차는 정지한다")
+    void stopsWhenStrategyReturnsFalse() {
         Car car = new Car("pobi");
 
         // 절대 이동하지 않는 전략
@@ -52,7 +57,8 @@ class CarTest {
     }
 
     @Test
-    void toString은_이름과_이동거리를_표시한다() {
+    @DisplayName("toString은 자동차 이름과 이동 거리를 '-'로 표시한다")
+    void toStringDisplaysNameAndDistance() {
         Car car = new Car("pobi");
 
         // 한 칸 이동시키기
