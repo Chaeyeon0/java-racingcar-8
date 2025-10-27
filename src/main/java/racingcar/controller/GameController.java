e606773207f2d9a0972b9d399b2440278eb1ed2d
qorcodusl <qorcodus03@naver.com>
2025-10-27 22:27:52 +0900

    refactor(controller) : RacingGame으로 게임 로직 위임하여 구조 개선

--- src/main/java/racingcar/controller/GameController.java
+++ src/main/java/racingcar/controller/GameController.java
@@ -1,9 +1,6 @@
 package racingcar.controller;
 
-import java.util.List;
-import racingcar.domain.Car;
-import racingcar.domain.Cars;
-import racingcar.domain.MovementStrategy;
+import racingcar.domain.RacingGame;
 import racingcar.domain.RandomMovementStrategy;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
@@ -12,21 +9,15 @@ public class GameController {
 
     public void run() {
         try {
-
             String carNamesInput = InputView.readCarNames();
             int tryCount = InputView.readTryCount();
 
-            Cars cars = new Cars(carNamesInput);
-            MovementStrategy strategy = new RandomMovementStrategy();
+            RacingGame game = new RacingGame(carNamesInput, new RandomMovementStrategy());
 
             System.out.println("실행 결과");
+            game.play(tryCount);
 
-            for (int i = 0; i < tryCount; i++) {
-                cars.moveAll(strategy);
-                OutputView.printRoundResult(cars.getCars());
-            }
-            List<Car> winners = cars.findWinners();
-            OutputView.printWinners(winners);
+            OutputView.printWinners(game.getWinners());
 
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] " + e.getMessage());
@@ -34,3 +25,4 @@ public class GameController {
         }
     }
 }
+
